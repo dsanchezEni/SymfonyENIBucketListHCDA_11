6a838451abde85603d3f7cba195af4ab64491c9f
dsanchez <dsanchez@campus-eni.fr>
2024-10-02 10:04:58 +0200

    Module 8 - TP2 - Mise en place des commentaires

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
 #[UniqueEntity(fields: 'email',message: 'There is already an account with this email')]
 #[UniqueEntity(fields: 'username',message: 'There is already an account with this username')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
