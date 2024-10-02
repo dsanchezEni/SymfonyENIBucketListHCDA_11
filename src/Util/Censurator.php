af02b497263a5ffed0040957f2898f1567526c45
dsanchez <dsanchez@campus-eni.fr>
2024-10-02 13:57:04 +0200

    Module 9 - TP2 - Mise en place d'un service Censurator avec gestion d'un fichier externe permettant de censurer des mots interdits et de les remplacer par des *****

--- src/Util/Censurator.php
+++ src/Util/Censurator.php
@@ -2,17 +2,38 @@
 
 namespace App\Util;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBag;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+
 class Censurator
 {
     //Constante contenant un tableau de mot censuré.
-    const UNWANTED_WORDS=["casino","viagra","bad","banana"];
+    //const UNWANTED_WORDS=["casino","viagra","bad","banana"];
+
+
+    //On injecte ContainerBagInterface dans le constructeur
+    //pour permettre de récupérer le paramètre dans le fichier services.yaml
+    public function __construct(private readonly ContainerBagInterface $params)
+    {
+    }
+
     public function purify(?string $text):string
     {
-         foreach (self::UNWANTED_WORDS as $unwantedWord){
-             //Autant d'étoiles que de lettres dans le mot:
-             $replacement = str_repeat("*",mb_strlen($unwantedWord));
-             $text=str_ireplace($unwantedWord,$replacement,$text);
-         }
+        $filename = $this->params->get('app.censurator_file');
+        if(file_exists($filename)) {
+            //On récupére les mots du fichier sous forme d'un tableau
+            $words = file($filename);
+
+            //foreach (self::UNWANTED_WORDS as $unwantedWord) {
+            foreach ($words as $unwantedWord) {
+                //Attention avant de traiter le mot,
+                // on retire le retour chariot présent en fin de chaque ligne
+                $unwantedWord = str_replace(PHP_EOL,'',$unwantedWord);
+                //Autant d'étoiles que de lettres dans le mot:
+                $replacement = str_repeat("*", mb_strlen($unwantedWord));
+                $text = str_ireplace($unwantedWord, $replacement, $text);
+            }
+        }
          return $text;
     }
 }
